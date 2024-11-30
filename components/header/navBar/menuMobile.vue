7a870c71a8bc46ae90233b6f0654692b3496cbe7
Nikita <TsarevNF@yandex.ru>
2024-11-30 21:59:10 +0500

    application initialization

---
+++ components/header/navBar/menuMobile.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="space-y-1 px-2 pb-3 pt-2">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <NuxtLink to="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+        aria-current="page">Главная</NuxtLink>
+      <NuxtLink to="/about"
+        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+        Резюме</NuxtLink>
+      <NuxtLink to="/projects"
+        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+        Проекты</NuxtLink>
+      <NuxtLink to="/contacts"
+        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+        Контакты</NuxtLink>
+      <NuxtLink to="/"
+        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+        Достижения</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col min-h-[100vh]">
+    <HeaderNavBar />
+    <main class="flex-initial h-full">
+      <slot />
+    </main>
+    <FooterInfo />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
