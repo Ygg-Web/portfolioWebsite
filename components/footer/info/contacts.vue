4a17acba4b7ccae14fb9b40b412e6eaa7b95b630
Nikita <TsarevNF@yandex.ru>
2024-12-08 22:00:24 +0500

    Работа над Footer сайта

---
+++ components/footer/info/contacts.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid gap-1 text-center text-[#B8B799]">
+    <div
+      v-for="(name, ind) in items"
+      :key="ind"
+      class="cursor-default"
+    >
+      <div class="flex justify-center">
+        <div class="footer-link relative">
+          {{ name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  items: string[];
+}>();
+</script>
+
+<style scoped lang="postcss">
+.footer-link {
+  @apply after:content-[''] after:absolute after:h-[1px] after:right-0 after:bottom-0 after:w-full after:bg-[#B8B799] after:transition-colors after:duration-500 transition-colors duration-500
+}
+.footer-link:hover {
+  @apply hover:after:content-[''] hover:after:absolute hover:after:h-[1px] hover:after:right-0 hover:after:bottom-0 hover:after:w-full hover:after:bg-white hover:after:transition-colors hover:after:duration-500 text-white transition-colors duration-500
+}
+</style>
